68dcb77a3532d2f8f4c397ed729197d59f3f35ad
Joel <jcbelgard@gmail.com>
2019-09-15 02:17:34 -0500

    Working on room edit tools

---
+++ Assets/Scripts/Tools/RoomTools/Editor/TileToolEditor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TileTool))]
+public class TileToolEditor : Editor
+{
+    private TileTool _tileTool;
+
+
+    public override void OnInspectorGUI() {
+        base.OnInspectorGUI();
+
+        _tileTool = (TileTool)target;
+
+        if(GUILayout.Button("Create Row")) {
+
+            _tileTool.CreateRow();
+
+        }
+
+        if (GUILayout.Button("Colorize Tiles")) {
+
+            _tileTool.ColorizeTiles();
+
+        }
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/Tools/RoomTools/TileTool.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileTool : MonoBehaviour
+{
+    [Header("Size")]
+    [Tooltip("The width in unity units for creating rows")]
+    public float width;
+
+    [Tooltip("The height in unity units for creating columns")]
+    public float height;
+
+
+    [Header("Colors")]
+    public List<Color> colors = new List<Color>();
+    
+
+
+    [Header("Location")]
+    public Transform startPosition;
+
+
+    [Header("Tile Pool")]
+    public List<GameObject> tiles = new List<GameObject>();
+
+    [Header("Currently Selected Sprites")]
+    public List<SpriteRenderer> currentSprites = new List<SpriteRenderer>();
+
+
+    private GameObject GetRandomTile() {
+        int randomIndex = Random.Range(0, tiles.Count);
+
+        return tiles[randomIndex];
+    }
+
+
+    public void ColorizeTiles() {
+        int count = currentSprites.Count;
+        for (int i = 0; i < count; i++) {
+            int randomColorIndex = Random.Range(0, colors.Count);
+
+            currentSprites[i].color = colors[randomColorIndex];
+        }
+    }
+
+    public void CreateRow() {
+        currentSprites.Clear();
+
+        float totalWidth = 0;
+
+        while (totalWidth < width) {
+
+            GameObject currentTile = Instantiate(GetRandomTile(), transform) as GameObject;
+            SpriteRenderer currentSprite = currentTile.GetComponentInChildren<SpriteRenderer>();
+            currentSprites.Add(currentSprite);
+
+            float spriteWidth = GetSpriteWidth(currentSprite);
+
+            Vector2 desiredPosition = new Vector2(startPosition.position.x + totalWidth, startPosition.position.y);
+
+            currentTile.transform.localPosition = desiredPosition;
+            totalWidth += spriteWidth;
+
+        }
+
+
+    }
+
+
+
+
+    public float GetSpriteWidth(SpriteRenderer sprite) {
+        return sprite.bounds.size.x;
+    }
+
+    public float GetSpriteHeight(SpriteRenderer sprite) {
+        return sprite.bounds.size.y;
+    }
+
+}
